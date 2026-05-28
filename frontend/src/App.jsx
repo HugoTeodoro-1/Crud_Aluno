8052e66ec9bb0dfef0783461e566cbfcbe08388e
HugoTeodoro-1 <hugopteodoro@gmail.com>
2026-05-28 20:29:38 +0000

    2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,19 +11,48 @@ function App(){
   const [editando, setEditando] = useState(false)
   const [idAtual, setIdAtual] = useState(null)
 
+  async function buscarAlunos(){
+    const resposta = await axios.get('https://turbo-space-adventure-7v47qp5676grcx7vw-3001.app.github.dev/alunos')
+    setAlunos(resposta.data)
+  }
+
+  useEffect(()=>{buscarAlunos();},[])
+
   async function salvar(e){
     e.preventDefault()
-    const aluno = [nome, email, curso]
+    const aluno = {nome, email, curso}
 
     if(editando){
-      await axios.put(`https://turbo-space-adventure-7v47qp5676grcx7vw-3001.app.github.dev/${idAtual}`, aluno)
+      await axios.put(`https://turbo-space-adventure-7v47qp5676grcx7vw-3001.app.github.dev/alunos/${idAtual}`, aluno)
       setEditando(false)
       setIdAtual(null)
     } else{
-      await axios.post("https://turbo-space-adventure-7v47qp5676grcx7vw-3001.app.github.dev/", aluno)
+      await axios.post("https://turbo-space-adventure-7v47qp5676grcx7vw-3001.app.github.dev/alunos", aluno)
       //limparFormulario()
       //buscarAlunos()
     }
+
+    limparFormulario()
+    buscarAlunos()
+  }
+
+  async function excluir(id){
+    await axios.delete(`https://turbo-space-adventure-7v47qp5676grcx7vw-3001.app.github.dev/alunos/${id}`)
+    buscarAlunos()
+  }
+
+  function limparFormulario(){
+    setNome('')
+    setEmail('')
+    setCurso('')
+  }
+
+  function editar(aluno){
+    setNome(aluno.nome)
+    setEmail(aluno.email)
+    setCurso(aluno.curso)
+    setIdAtual(aluno.id)
+    setEditando(true)
   }
 
   return(
@@ -56,7 +85,23 @@ function App(){
           {editando ? "Atualizar" : "Cadastrar"}
         </button>
       </form>
-    </div>
+
+      <hr/>
+
+
+      {
+        alunos.maps((aluno)=>(
+          <div key={aluno.id}>
+            <h3>{aluno.nome}</h3>
+            <p>{aluno.email}</p>
+            <p>{aluno.curso}</p>
+            <button onClick={()=> editar(aluno)}>Editar</button>
+            <button onClick={()=> excluir(aluno.id)}>Excluir</button>
+          <hr/>
+          </div>
+        ))
+      }
+      </div>
   )
 }
 
